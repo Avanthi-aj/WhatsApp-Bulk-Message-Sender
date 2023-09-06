5509b0b860e105d845b58b0807e26e371823a9c3
Avanthika <84899743+Avanthi-aj@users.noreply.github.com>
2023-09-06 19:20:33 +0530

    Update message.py

--- Whatsapp-main/Whatsapp-main/message.py
+++ Whatsapp-main/Whatsapp-main/message.py
@@ -1,19 +1,13 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
-from selenium.webdriver.chrome.options import Options
-
-option = webdriver.ChromeOptions()
-option.add_argument("--user-data-dir=/home/username/.config/google-chrome")
-
-driver = webdriver.Chrome(options=option)     
-
+driver = webdriver.Chrome()     
 driver.get('https://web.whatsapp.com')
 input("Press ENTER...")
+
 with open('message.txt', 'r') as file:
     msg = file.read()
 
@@ -24,12 +18,12 @@ with open('numbers.txt', 'r') as file:
         num = n.rstrip()
         link = f'https://web.whatsapp.com/send/?phone={num}'
         driver.get(link)
-        time.sleep(10)
+        time.sleep(20)
         actions = ActionChains(driver)
         for line in msg.split('\n'):
             actions.send_keys(line)
             actions.key_down(Keys.SHIFT).send_keys(Keys.ENTER).key_up(Keys.SHIFT)
         actions.send_keys(Keys.ENTER)
         actions.perform()
-        time.sleep(5)
+        time.sleep(10)
 driver.quit()
